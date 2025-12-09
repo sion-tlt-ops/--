384c6ac2aef427dcda972ee6bd3fb5b597167e60
sion-tlt-ops <sion-tlt@mail.ru>
2025-12-09 20:07:59 +0700

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Ghost)
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+basic.forever(function () {
+    if ("" == "" && "" == "") {
+    	
+    } else if ("" == "" && "" == "") {
+    	
+    } else if ("" == "" && "" == "") {
+    	
+    }
+})
